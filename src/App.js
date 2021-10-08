0f9dcc0cccc185029e82d140e2456a9cc81ce71f
pnchinmay <pnchinmay9@gmail.com>
2021-10-09 01:38:58 +0530

    Using onClick and callback functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./styles.css";
 
 var bgColor = "skyblue";
-var uName = prompt("Enter username");
+var likeCounter = 0;
+// var uName = prompt("Enter username");
 
 export default function App() {
+  function likeClickHandler() {
+    console.log("liked");
+    likeCounter++;
+    console.log(likeCounter);
+    likeCounterDisplay.innerText = "Liked {likeCounter} times";
+  }
+
   return (
     <div className="App">
-      <h1 style={{ backgroundColor: bgColor }}>Welcome {uName}</h1>
+      {/* <h1 style={{ backgroundColor: bgColor }}>Welcome {uName}</h1> */}
+      <h1>Welcome</h1>
+      <div id="likeCounterDisplay">Liked {likeCounter} times !!!</div>
+      <button onClick={likeClickHandler}>Like 👍</button>
     </div>
   );
 }
