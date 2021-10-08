d949a8c7655983b7849795a0afa45d53fe0a12bf
pnchinmay <pnchinmay9@gmail.com>
2021-10-09 01:37:44 +0530

    Using prompt from JSX - live exercise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 
+var bgColor = "skyblue";
+var uName = prompt("Enter username");
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 style={{ backgroundColor: bgColor }}>Welcome {uName}</h1>
     </div>
   );
 }
