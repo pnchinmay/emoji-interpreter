725658f8bcab5d7040788c23171f9a7bd6b19d5b
pnchinmay <pnchinmay9@gmail.com>
2021-10-10 00:31:25 +0530

    Refactor: cleaning canvas and live exercise - method 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-// var bgColor = "skyblue";
-// var uName = prompt("Enter username");
-
 export default function App() {
+
   const [likeCounter, setLikeCounter] = useState(0);
 
-  function likeClickHandler() {
-    // var newLikeCounter = likeCounter + 1;
-    // setLikeCounter(newLikeCounter);
-    setLikeCounter(likeCounter + 1);
-  }
+  // function likeClickHandler() {
+  //   setLikeCounter(likeCounter + 1);
+  // }
 
-  // This is being put in console from view and not from the onClickHandler callback function
-  console.log("LikeCounter: ", likeCounter);
 
   return (
     <div className="App">
       {/* <h1 style={{ backgroundColor: bgColor }}>Welcome {uName}</h1> */}
       <h1>Welcome</h1>
+      {/* <button onClick={likeClickHandler}>Like 👍</button> */}
+      <button onClick={() => setLikeCounter(likeCounter + 1)}>Like 👍</button>
       <div id="likeCounterDisplay">Liked {likeCounter} times !!!</div>
-      <button onClick={likeClickHandler}>Like 👍</button>
     </div>
   );
 }
