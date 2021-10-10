bacee1e42aa8648aa281b284a286639a0cd9a7ca
pnchinmay <pnchinmay9@gmail.com>
2021-10-10 12:15:04 +0530

    Handle data from input element

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var [usrInput, setUsrInput] = useState("");
+  function inputHandler(event) {
+    setUsrInput(event.target.value);
+  }
   return (
     <div className="App">
       <h1>Welcome</h1>
+      <input onChange={inputHandler}></input>
+      <div>Welcome {usrInput} </div>
     </div>
   );
 }
