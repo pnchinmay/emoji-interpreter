421394a01ac94b836338e0d861bf2107501fce54
pnchinmay <pnchinmay9@gmail.com>
2021-10-10 12:07:39 +0530

    CANVAS Cleaned

--- src/App.js
+++ src/App.js
@@ -2,21 +2,9 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-
-  const [likeCounter, setLikeCounter] = useState(0);
-
-  // function likeClickHandler() {
-  //   setLikeCounter(likeCounter + 1);
-  // }
-
-
   return (
     <div className="App">
-      {/* <h1 style={{ backgroundColor: bgColor }}>Welcome {uName}</h1> */}
       <h1>Welcome</h1>
-      {/* <button onClick={likeClickHandler}>Like 👍</button> */}
-      <button onClick={() => setLikeCounter(likeCounter + 1)}>Like 👍</button>
-      <div id="likeCounterDisplay">Liked {likeCounter} times !!!</div>
     </div>
   );
 }
